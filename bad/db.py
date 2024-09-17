c26f3b848c3a75a9c1e35fbdca3448a324c913b1
Pritesh Chandaliya <priteshchandaliya@gmail.com>
2024-09-17 13:27:03 -0700

    updated the failure int from 0 to 1

--- bad/db.py
+++ bad/db.py
@@ -5,7 +5,7 @@ from passlib.hash import pbkdf2_sha256
 def db_init():
 
     users = [
-        ('admin', pbkdf2_sha256.encrypt('123456')),
+        ('admin', pbkdf2_sha256.encrypt('123456789')),
         ('john', pbkdf2_sha256.encrypt('Password')),
         ('tim', pbkdf2_sha256.encrypt('Vaider2'))
     ]
@@ -16,7 +16,7 @@ def db_init():
     c.execute("CREATE TABLE users (user text, password text, failures int)")
 
     for u,p in users:
-        c.execute("INSERT INTO users (user, password, failures) VALUES ('%s', '%s', '%d')" %(u, p, 0))
+        c.execute("INSERT INTO users (user, password, failures) VALUES ('%s', '%s', '%d')" %(u, p, 1))
 
     conn.commit()
     conn.close()
