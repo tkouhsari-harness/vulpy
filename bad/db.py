a3ae836a979a233e173caa47ad333a2c8130fa75
Pritesh Chandaliya <priteshchandaliya@gmail.com>
2024-09-23 10:23:01 -0700

    Update bad/db.py
    LGTM
    
    Co-authored-by: harness-ai-bot <pritesh.chandaliya+unscripted@harness.io>

--- bad/db.py
+++ bad/db.py
@@ -16,7 +16,7 @@ def db_init():
     c.execute("CREATE TABLE users (user text, password text, failures int)")
 
     for u,p in users:
-        c.execute("INSERT INTO users (user, password, failures) VALUES ('%s', '%s', '%d')" %(u, p, 1))
+        c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES (?, ?, ?, ?, ?)", (u, p, 0, 0, ''))
 
     conn.commit()
     conn.close()
